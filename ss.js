9fe01b65501296b130d21c6daf7cd3df9eb0d50b
Prakit <saratgogoi78@gmail.com>
2024-09-21 12:53:38 +0530

    Delete ss.js

--- ss.js
+++
@@ -1,7 +0,0 @@
-document.getElementById("get-exploits").addEventListener("click", function() {
-	window.location.href = "YOUR_URL_HERE"; /* Set your URL here */
-});
-
-document.getElementById("get-started").addEventListener("click", function() {
-		window.location.href = "YOUR_URL_HERE"; /* Set your URL here */
-});
