04789d11212082140d7fdef484d78ae4e44ed460
Prakit <saratgogoi78@gmail.com>
2024-09-21 11:40:57 +0530

    Update ss.js

---
+++ ss.js
@@ -0,0 +1,13 @@
+// Add event listeners to the buttons
+const getStartedButton = document.getElementById('get-started-button');
+const viewExploitsButton = document.getElementById('view-exploits-button');
+
+getStartedButton.addEventListener('click', () => {
+    // Replace with your URL
+    window.location.href = 'https://prakit1234.github.io/UTPALX/';
+});
+
+viewExploitsButton.addEventListener('click', () => {
+    // Replace with your URL
+    window.location.href = 'https://example.com/exploits';
+});
